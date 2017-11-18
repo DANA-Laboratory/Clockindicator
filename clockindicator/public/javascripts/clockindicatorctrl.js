56286c5d1c6efe955731a72c572758cd922fa789
rafzalan <rafzalan@gmail.com>
2017-11-18 06:57:04 +0330

    progress

--- clockindicator/public/javascripts/clockindicatorctrl.js
+++ clockindicator/public/javascripts/clockindicatorctrl.js
@@ -8,16 +8,16 @@ app.controller("ClockIndicatorInputCtrl", ['$scope', 'clockIndicatorResourceServ
     $scope.config = {};
     $scope.charts = [];
     $scope.load = function () {
-        var oldvalue = "";
-        var max = 10;
-        for (i = 0; i < 10; i++) {
-            cirs.get({ id: i }, function (variable) {
-                $scope.config[variable.id] = variable;
-                if (($scope.config).length == max) {
-                    $scope.$broadcast('variables_loaded');
-                }
-            });
-        }
+      var oldvalue = "";
+      var max = 10;
+      for (i = 0; i < 10; i++) {
+        cirs.get({ id: i }, function (variable) {
+          $scope.config[variable.id] = variable;
+          if (($scope.config).length == max) {
+            $scope.$broadcast('variables_loaded');
+          }
+        });
+      }
     }
     $scope.clicked = function(event) {
       if(event.currentTarget){
@@ -77,13 +77,39 @@ app.controller("ClockIndicatorCtrl", ['$scope', 'clockIndicatorResourceService',
         var ctx = document.getElementById(key);
         progressChart[key] = new Chart(ctx, {
             type: 'progressIndicator',
-            data: {},
-            options: {},
+            data: {
+                value: 50,
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                color: "GreenYellow",
+                fillcolor: "DarkBlue",
+                textcolor: "GreenYellow",
+            }
         });
-        progressChart[key].drawing(null);
         chartId++;
     }
-    console.log(ctx);
 
     var getConf = function (canvasId) {
         //config have changed, try redraw
